1519729d0bfb664fb168604e2645ac81eb2b3f54
Domantas K <130393253+Dogzocute-D-e-v@users.noreply.github.com>
2023-10-21 22:46:56 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,6 @@ php artisan down
 curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
 chmod -R 755 storage/* bootstrap/cache
 composer install --no-dev --optimize-autoloader
-yes
 php artisan view:clear
 php artisan config:clear
 php artisan migrate --seed --force
