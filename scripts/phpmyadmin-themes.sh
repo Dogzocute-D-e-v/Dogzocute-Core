ea8689cb2128a9bda9f6ea10d8aa8686c1946847
Domantas K <dogzocute+GitHub@gmail.com>
2024-06-16 15:26:20 +0200

    Create phpmyadmin-themes.sh

---
+++ scripts/phpmyadmin-themes.sh
@@ -0,0 +1,19 @@
+cd /var/www/phpmyadmin/themes
+curl -O https://files.phpmyadmin.net/themes/boodark-teal/1.1.0/boodark-teal-1.1.0.zip
+unzip boodark-teal-1.1.0.zip
+rm -r boodark-teal-1.1.0.zip
+curl -O https://files.phpmyadmin.net/themes/boodark-orange/1.1.0/boodark-orange-1.1.0.zip
+unzip boodark-orange-1.1.0.zip
+rm -r boodark-orange-1.1.0.zip
+curl -O https://files.phpmyadmin.net/themes/boodark-nord/1.1.0/boodark-nord-1.1.0.zip
+unzip boodark-nord-1.1.0.zip
+rm -r boodark-nord-1.1.0.zip
+curl -O https://files.phpmyadmin.net/themes/boodark/1.1.0/boodark-1.1.0.zip
+unzip boodark-1.1.0.zip
+rm -r boodark-1.1.0.zip
+curl -O https://files.phpmyadmin.net/themes/blueberry/1.1.0/blueberry-1.1.0.zip
+unzip blueberry-1.1.0.zip
+rm -r blueberry-1.1.0.zip
+curl -O https://files.phpmyadmin.net/themes/darkwolf/5.2/darkwolf-5.2.zip
+unzip darkwolf-5.2.zip
+rm -r darkwolf-5.2.zip
